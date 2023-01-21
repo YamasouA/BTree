1ddc165f77ac007b8f6f3515b30bee4834752aac
YamasouA <akiakiskyhand@gmail.com>
2023-01-22 00:54:53 +0900

    insertNode, deleteItem, print

--- BTree.java
+++ BTree.java
@@ -9,7 +9,7 @@ public class BTree {
     private boolean undersize;
 
     private class Page {
-        int n = 0;
+        int n = 0; // データ数
         int[] key = new int[2 * M];
         Page[] branch = new Page[2 * M + 1];
 
@@ -24,8 +24,9 @@ public class BTree {
             return branch[i].searchNode(searchKey);
         }
 
+        // i: 何番目に値を入れるか
         private void insertItem(int i, int newKey, Page newPage) {
-            // keyを見るついでに右に動かす
+            // 右に値を動かす
             for (int j = n; j > i; j--) {
                 branch[j + 1] = branch[j];
                 key[j] = key[j - 1];
@@ -57,7 +58,63 @@ public class BTree {
             n--;
             insertPage = q;
         }
+
+        private boolean insertNode() {
+            int i = 0;
+            while (i < n && key[i] < insertKey)
+                i++;
+            if (i < n && key[i] == insertKey) {
+                message = "もう登録されています";
+                return true;
+            }
+            // 子ノードにキーが存在する
+            if (branch[i] != null && branch[i].insertNode())
+                return true;
+            // キーが存在しない && データが満杯じゃない
+            if (n < 2 * M) {
+                insertItem(i, insertKey, insertPage);
+                return true;
+            } else {
+                split(i);
+                return false;
+            }
+        }
+
+        // key[i], branch[i + 1]を削除する。ページが小さくなり過ぎるとundersizeフラグを立てる
+        private void deleteItem(int i) {
+            while (++i < n) {
+                key[i - 1] = key[i];
+                branch[i] = branch[i + 1];
+            }
+            branch[n] = null;
+            undersize = (--n < M);
+        }
+
+        // branch[i - 1]の一番右をkey[i - 1]を経由してbranch[i]に動かす
+        private void moveRight(int i) {
+            Page left = branch[i - 1];
+            Page right = branch[i];
+            right.insertItem(0, key[i - 1], right.branch[i]);
+            key[i - 1] = left.key[left.n - 1];
+            right.branch[0] = left.branch[left.n];
+            left.n--;
+        }
+
+        private void print() {
+            System.out.print("(");
+            for (int i = 0; i <= n; i++) {
+                if (branch[i] == null)
+                    System.out.print(".");
+                else:
+                    branch[i].print();
+                if (i < n)
+                    System.out.print(key[i]);
+            }
+            System.out.print(")");
+        }
     }
+    private Page root = new Page();
+    static String message = "";
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
